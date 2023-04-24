5123a1d20f50904a6894e7eafdc1bbaa4550c3ef
ashrafsharif <acopkirk@gmail.com>
2023-04-24 21:12:41 +0800

    updated the php test page

--- gtp/index.php
+++ gtp/index.php
@@ -1,31 +1,50 @@
 <?php
 
-$mysql_host='';
-$mysql_port=3306;
-$mysql_user='gtpadmin';
-$mysql_pass='gtpRDSpassword2023';
-$mysql_db='gtp';
-$redis_host='';
-$redis_port=6379;
+$redis_host ="";
+$redis_port ="6379";
+$mysql_host = "";
+$mysql_user = "";
+$mysql_pass = "";
+$mysql_db = "gtp";
 
-$pdo = new PDO("mysql:host=" . $mysql_host . ":" . $mysql_port . ";dbname=" . $mysql_db . ", " . $mysql_user . ", " . $mysql_pass);
-$now = $pdo->query('SHOW VARIABLES LIKE "%version%"')->fetchColumn();
+echo "<span><center>";
+echo "\n=====MySQL=======\n";
+echo "<br>";
+try {
+  $conn = new PDO("mysql:host=$mysql_host;dbname=$mysql_db", $mysql_user, $mysql_pass);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "\nMySQL Connected successfully";
+  echo "<br>";
+} catch(PDOException $e) {
+  echo "\nConnection failed: " . $e->getMessage();
+}
+echo "\n==============\n";
+echo "<br>";
+echo "<br>";
 
-echo "=====MYSQL=======";
-echo $now;
-echo "=========";
+echo "\n====Redis=====\n";
+echo "<br>";
+$redis = new Redis();
+$redis->connect($redis_host, $redis_port);
+echo "\nConnection to server sucessfully\n";
+echo "<br>";
+echo "\nServer is running: ".$redis->ping();
+echo "<br>";
+echo "\nTrying to set a key in Redis..";
+$redis->set("my-project", "gtp");
+echo "<br>";
+echo "\nStored string in redis: " .$redis->get("my-project");
+echo "<br>";
+echo "\n=======\n";
+echo "<br>";
+echo "<br>";
 
-echo "====PHP=======";
+echo "\n====PHP=======\n";
+echo "<br>";
 phpinfo(INFO_MODULES);
-echo "==========";
+echo "<br>";
+echo "\n==========\n";
+echo "</center></span>";
 
-echo "====Redis=====";
-$redis = new Redis(); 
-$redis->connect($redis_host, $redis_port); 
-echo "Connection to server sucessfully"; 
-echo "Server is running: ".$redis->ping(); 
-
-$redis->set("tutorial-name", "Redis tutorial");
-echo "Stored string in redis:: " .$redis→get("tutorial-name"); 
-echo "=======";
 ?>
